b2fb5b89625e57e53a6e4b4cf868cdeafd3902bf
Anton Kravchenko <78639975+AAxelx@users.noreply.github.com>
2024-09-30 19:36:14 +0300

    CRF-1: added models and connection to config file

---
+++ ConferenceRoomBooking.Common/Models/RoomModel.cs
@@ -0,0 +1,10 @@
+namespace ConferenceRoomBooking.Common.Models;
+
+public class RoomModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public int Capacity { get; set; }
+    public decimal BasePricePerHour { get; set; }
+    public List<ServiceModel> AvailableServices { get; set; } = new List<ServiceModel>();
+}
